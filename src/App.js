c621328fa65461392f5f0bc43aa3f6529aedb977
RahulD-MG <rahuldey@mind-graph.com>
2022-01-12 12:00:57 +0530

    assignment 1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
